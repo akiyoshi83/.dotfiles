5d3f17e01a0b7c4509b152805af2012bdddaae6d
akiyoshi83 <mail@akiyoshi.info>
2015-04-29 07:27:44 +0900

    Add including .gitconfig at install script

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ git clone https://github.com/akiyoshi83/.dotfiles.git
 
 echo "source \$HOME/.dotfiles/.profile" >> ~/.bashrc
 
-ln -s ~/.dotfiles/.gemrc ~/
-
 # vim
 ln -s ~/.dotfiles/.vimrc ~/
 ln -s ~/.dotfiles/.vim   ~/
@@ -15,3 +13,13 @@ mkdir -p ~/.vim/tmp
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 vim +":NeoBundleInstall" +:q
 (cd ~/.dotfiles/.vim/bundle/vimproc/ && make)
+
+# gem
+ln -s ~/.dotfiles/.gemrc ~/
+
+# git
+cat << EOF >> ~/.gitconfig
+[include]
+	path = ~/.dotfiles/.gitconfig
+EOF
+
