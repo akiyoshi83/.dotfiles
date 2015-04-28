03d96717452d4cf5b3cd527a69643c41ee20a4c7
akiyoshi83 <mail@akiyoshi.info>
2015-04-29 07:26:43 +0900

    Make .vim/tmp directory at install script

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ ln -s ~/.dotfiles/.gemrc ~/
 # vim
 ln -s ~/.dotfiles/.vimrc ~/
 ln -s ~/.dotfiles/.vim   ~/
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/tmp
 
 # NeoBundle
-mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 vim +":NeoBundleInstall" +:q
 (cd ~/.dotfiles/.vim/bundle/vimproc/ && make)
